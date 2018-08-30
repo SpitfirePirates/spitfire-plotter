3fb91cd55c4917f5b789b31ae02cc4ab08aa46bd
MoeBrowne <oliver.browne@uplyme.com>
2018-08-30 20:31:51 +0100

    Calculates the board width in steps from mm and makes use of the hypot function

--- Plotter.js
+++ Plotter.js
@@ -6,16 +6,25 @@ class Plotter
 
     constructor() {
         this.position = { x: 0, y: 0 }
-        this.board = { width: 300, height: 300 }
+
+	const motorDistance = 600; //mm
+	const gearDiameter = 49.81; //mm
+	const gearCircumference = Math.PI*gearDiameter; //mm
+	const motorDistanceRotations = motorDistance/gearCircumference;
+	const boardWidthSteps = (motorDistanceRotations/motorDistance) * 4076; //steps
+
+        this.board = { width: boardWidthSteps, height: boardWidthSteps }
+
         this.leftMotor = new LeftMotor(0)
         this.rightMotor = new RightMotor(this.board.width)
     }
 
+    // move relative to the current position
     move(x, y) {
-        const absx = this.position.x + x;
-        const absy = this.position.y + y;
-        const leftHypo = Math.sqrt(Math.pow(absx, 2) + Math.pow(absy, 2))
-        const rightHypo = Math.sqrt(Math.pow(this.board.width - absx, 2) + Math.pow(absy, 2))
+        const absx = this.position.x + x
+	const absy = this.position.y + y
+        const leftHypo = Math.hypot(absx, absy)
+        const rightHypo = Math.hypot(this.board.width - absx, absy)
 
         this.leftMotor.setLength(leftHypo)
         this.rightMotor.setLength(rightHypo)
