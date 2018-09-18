9d456fb9ba290edfb4102a93cc5e18511492b03e
Joe Strong <joestrong@gmail.com>
2018-09-18 13:37:18 +0100

    Adds walker function

--- main.js
+++ main.js
@@ -9,10 +9,20 @@ async function run () {
 
     // shapes.square();
 
-    await Shapes.circle(10, 0.5)
-    await Shapes.circle(10, 0.5)
-    await Shapes.circle(10, 0.5)
-    await Shapes.circle(10, 0.5)
+    // await Shapes.circle(10, 0.5)
+    // await Shapes.circle(10, 0.5)
+    // await Shapes.circle(10, 0.5)
+    // await Shapes.circle(10, 0.5)
+
+    const points = [
+        { x: 100, y: 100},
+        { x: 200, y: 200},
+        { x: 800, y: 200},
+        { x: 800, y: 600}
+    ]
+
+    Walker.walkToCenter()
+    Walker.walk(points)
 
     plotter.release()
 }
