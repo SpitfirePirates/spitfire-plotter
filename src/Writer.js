e5ea83f45235c30786f4a48fcb9e00340ec8d9ca
Joe Strong <joestrong@gmail.com>
2020-02-07 13:34:52 +0000

    Adds font size setting

--- src/Writer.js
+++ src/Writer.js
@@ -1,6 +1,6 @@
 const Plotter = require('./Plotter.js')
 const plotter = new Plotter()
-const TextToSVG = require('text-to-svg');
+const TextToSVG = require('text-to-svg')
 const SVGPathInterpolator = require('svg-path-interpolator')
 const InvalidTextException = require('./Exceptions/InvalidTextException')
 
@@ -16,9 +16,12 @@ function* makeWriteIterator(text) {
     return
 }
 
-async function write(text) {
+async function write(text, size) {
     if (!text) {
-        throw new InvalidTextException();
+        throw new InvalidTextException()
+    }
+    if (!size) {
+        size = 672
     }
     const walkIterator = makeWriteIterator(text)
     for (let {dx, dy} of walkIterator) {
@@ -26,20 +29,19 @@ async function write(text) {
     }
 }
 
-function textToPoints(text) {
-    const textToSVG = TextToSVG.loadSync(__dirname + '/../resources/font-ems-delight.ttf');
+function textToPoints(text, size) {
+    const textToSVG = TextToSVG.loadSync(__dirname + '/../resources/font-ems-delight.ttf')
+    const attributes = {fill: 'red', stroke: 'black'}
+    const options = {x: 0, y: 0, fontSize: size, anchor: 'top', attributes: attributes}
 
-    const attributes = {fill: 'red', stroke: 'black'};
-    const options = {x: 0, y: 0, fontSize: 672, anchor: 'top', attributes: attributes};
-
-    const svg = textToSVG.getPath(text, options);
+    const svg = textToSVG.getPath(text, options)
 
     const interpolator = new SVGPathInterpolator({
         joinPathData: true,
         minDistance: 0.5,
         roundToNearest: 0.25,
         sampleFrequency: 0.001
-    });
+    })
     const pathData = interpolator.processSvg(svg)
 
     return pathData
